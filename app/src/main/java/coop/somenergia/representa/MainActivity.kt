1ede26ef17b55f1c68b922b8e4fa337cafc5d33e
David García Garzón <david.garcia@somenergia.coop>
2023-12-30 20:58:39 +0100

    🏗 Android Template Empty View

---
+++ app/src/main/java/coop/somenergia/representa/MainActivity.kt
@@ -0,0 +1,11 @@
+package coop.somenergia.representa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
