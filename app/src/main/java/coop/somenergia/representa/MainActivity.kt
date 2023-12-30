e54d20ea6794db15148519881cb79e0696a4265f
David García Garzón <david.garcia@somenergia.coop>
2023-12-30 21:16:57 +0100

    ✨ loading the web in a webview

--- app/src/main/java/coop/somenergia/representa/MainActivity.kt
+++ app/src/main/java/coop/somenergia/representa/MainActivity.kt
@@ -1,11 +1,29 @@
 package coop.somenergia.representa
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://ov-representa.test.somenergia.coop")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.setSupportZoom(true)
+        webView.settings.setSupportMultipleWindows(true)
     }
 }
